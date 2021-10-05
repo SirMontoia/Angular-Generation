cf5ab88b2e1bace8434f7767cf94c07cc8217f6b
unknown <julioteixeirapimont@gmail.com>
2021-10-05 15:09:14 -0300

    Inserção de CRUD completo do Tema

--- BlogPessoalJulio/src/app/app.module.ts
+++ BlogPessoalJulio/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { SobrenosComponent } from './sobrenos/sobrenos.component';
 import { FormsModule } from '@angular/forms';
 import { InicioComponent } from './inicio/inicio.component';
 import { TemaComponent } from './tema/tema.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 
 @NgModule({
     declarations: [
@@ -24,6 +26,8 @@ import { TemaComponent } from './tema/tema.component';
     SobrenosComponent,
     InicioComponent,
     TemaComponent,
+    TemaEditComponent,
+    TemaDeleteComponent,
  
     
   ],
